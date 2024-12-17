5f755d780e388dc6d9e0879241ebadb193963e3c
Jeremy Ascher <79414199+jeremy-pelican@users.noreply.github.com>
2024-12-17 09:05:07 -0500

    Update App.js to include 12/16/24 components and new routing

--- Week1/12-13-24/my-app/src/App.js
+++ Week1/12-13-24/my-app/src/App.js
@@ -4,10 +4,19 @@ import EventDemoComponent from './EventDemoComponent';
 import Hello from './Hello';
 import PostComponent from './PostsComponent';
 import CommentComponent from './CommentComponent';
+//import ListandKeysComponent from './ListandKeysComponent';
+import ListandKeysComponent from './components/listandkeys/ListandKeysComponent';
+//import AxiosGetDemo from './AxiosGetDemo';
+import AxiosGetDemo from './components/axiosdemo/AxiosGetDemo';
+import AxiosPost from './components/axiosdemo/AxiosPost';
+import Navbar from './Navbar';
+import ToDoList from './components/todolist/ToDoList';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      {/* <Navbar/>
       <h2>Hello React</h2>
       <Hello firstname="Jasdhir" lastname="Singh"/>
       <Hello firstname="Jeremy" lastname="Ascher"/>
@@ -17,6 +26,22 @@ function App() {
       <hr/>
       <PostComponent name = "Jeremy Posted..." content = "React is so cool!"/>
       <CommentComponent/>
+      <hr/>
+      <ListandKeysComponent/>
+      <hr/>
+      <AxiosGetDemo/> */}
+      <Router>
+        <div className="app">
+          <Navbar/>
+        </div>
+        <Routes>
+          <Route path='/hello' element={<Hello/>}/>
+          <Route path='sdc' element={<StateDemoComponent/>}/>
+          <Route path='/getposts' element={<AxiosGetDemo/>}/>
+          <Route path='/comments' element={<CommentComponent/>}/>
+          <Route path='/todoList' element={<ToDoList/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
